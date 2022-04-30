60811a93203c0875fa2899efffe3bb6d85760432
piyuspatil21 <64527316+piyuspatil21@users.noreply.github.com>
2022-04-30 10:57:53 +0530

    Add files via upload

--- Human.java
+++ Human.java
@@ -3,30 +3,17 @@ package com.edu;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Human {
-	@Autowired
 	Heart heart;
-	
 
-	public Heart getHeart() {
-		return heart;
-	}
-
-
-	public void setHeart(Heart heart) {
-		this.heart = heart;
-	}
-
-
+	@Autowired
 	public Human(Heart heart) {
 		super();
 		this.heart = heart;
 	}
 	
-	
      public void humanFunc() {
-		System.out.println("Human function calling heart");
-	      heart.heartFunction();
-		
+     System.out.println("Human function calling heart");
+      heart.heartFunction();
 	}
 
 }
