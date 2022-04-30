7bce87af4bcca38db0346c3bfa4ac5d54b1d0e5f
piyuspatil21 <64527316+piyuspatil21@users.noreply.github.com>
2022-04-30 11:00:40 +0530

    Add files via upload

---
+++ SpellChecker.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+public class SpellChecker {
+	public SpellChecker() {
+		System.out.println("Inside Spellchecker constructor");
+	}
+
+	public void checkSpelling() {
+		System.out.println("Inside checkspelling");
+		
+	}
+
+}
